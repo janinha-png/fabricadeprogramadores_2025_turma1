1e368cf87a87307f8f76f37b2028ad6a909ff7ca
janinha <jjjoana726@gmail.com>
2025-07-31 10:09:45 -0300

    exercicios antes das ferias

---
+++ atividades.py
@@ -0,0 +1,25 @@
+def quadrado(lado):
+    area=lado**2
+    return area
+print(quadrado(30))
+
+def reajuste (salario):
+    novo=salario+salario*0.15
+    return novo
+print(reajuste(1000))  
+
+def triangulo(base,altura):
+    resultado=(base*altura/2)
+    return resultado
+print(triangulo(2,6))
+
+def conversor(c):
+    resposta=(9*c+160)/5
+    return resposta
+print(conversor(100))
+
+def equaçao(y,x):
+    resposta=(x,y)
+    return resposta
+print(equaçao (10,25))
+    
---
+++ funções.py
@@ -0,0 +1,53 @@
+"""texto_escrito="Este livro é legal!"
+
+outro_texto='Vou aprender a programar.'
+
+texto="""
+#primeira linha
+#segunda linha 
+#terceira linha
+"""
+
+print("\tOi!\nEu me chamo \"(Janinha)\"!!!")
+
+outro_texto=texto_escrito
+print(outro_texto)
+
+amora=23
+pessego=34
+amora += pessego
+print(amora)
+
+cuzcuz=10
+ovo=5
+cuzcuz -= ovo
+print(cuzcuz)
+
+banana=20
+choco=10
+banana /= choco
+print(banana)
+
+salsicha=3
+pao=3
+salsicha *= pao
+print(salsicha)
+
+carteira_digital=0
+print(carteira_digital)
+carteira_digital += 100
+print(carteira_digital)
+carteira_digital -= 75.99
+print(carteira_digital)
+carteira_digital += 200
+print(carteira_digital)
+
+"""
+nome=input("insira seu nome:")
+saudaçao="ola!"+ nome
+print(saudaçao)
+
+numero_string=input("insira um numero:")
+numero_inteiro=int(numero_string)
+subtraçao=numero_inteiro-2
+print(subtraçao)
