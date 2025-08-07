053596b89e84e577e24f48f68ba972a2ea96514a
janinha <jjjoana726@gmail.com>
2025-08-07 10:08:41 -0300

    meu_primeiro_programa.py

---
+++ meu_primeiro_programa.py
@@ -0,0 +1,50 @@
+print("ola joana")
+print(1500+(1500*5/100))
+print(10+20*30)
+print(4**2/30)
+print((9**4+2)*6-1)
+salario=1500
+aumento=5
+print(salario+(salario*aumento/100))
+a=3
+b=5
+print(2*a*3*b)
+a=4
+b=10
+c=5.0
+d=1
+f=5
+print(a==c)
+print(a<b)
+print(d>b)
+print(c!=f)
+print(a==b)
+print(c<d)
+print(b>a)
+print(c>=f)
+print(f>=c)
+print(c<=c)
+print(c<=f)
+print('-----x-----')
+a=True
+b=False
+print(not a)
+print(a or b)
+print(b and b)
+print('-----x-----')
+a=True
+b=False
+c=True
+print(a and a)
+print(b and b)
+print(not c)
+print(not b)
+print(not a)
+print(a and b)
+print(b and c)
+print(a or c)
+print(b or c)
+print(c or a)
+print(c or b)
+print(c or c)
+print(b or b)
