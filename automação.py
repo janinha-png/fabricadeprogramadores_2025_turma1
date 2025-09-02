a7405c681b9a0c5808f52e1a091c97598b89ada3
janinha <jjjoana726@gmail.com>
2025-09-02 10:23:38 -0300

    Add files via upload

---
+++ automação.py
@@ -0,0 +1,32 @@
+import mouseinfo, pyautogui
+
+
+
+mouseinfo.mouseInfo()
+pyautogui.moveTo(1811,10, duration=1)
+pyautogui.click()
+pyautogui.moveTo(797,1053, duration=1)
+pyautogui.click()
+
+pyautogui.write("Pikachu")
+pyautogui.press("enter")
+
+pyautogui.moveTo(709,116, duration=1)
+pyautogui.click()
+
+pyautogui.write("irmao do jorel")
+pyautogui.press("enter")
+
+pyautogui.moveTo(697,1049, duration=1)
+pyautogui.click()
+pyautogui.moveTo(718,544, duration=1)
+pyautogui.click()
+
+pyautogui.moveTo(415,648, duration=1)
+pyautogui.click()
+pyautogui.moveTo(834,641, duration=1)
+pyautogui.click()
+pyautogui.moveTo(412,832, duration=1)
+pyautogui.click()
+pyautogui.moveTo(858,938, duration=1)
+pyautogui.click()
